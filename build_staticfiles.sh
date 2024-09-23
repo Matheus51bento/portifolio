8b3d5e855f7664d24b54385530cdff6e08a92b2b
Matheus Bento <matheus51bento@gmail.com>
2024-09-23 15:47:26 -0300

    vercel config

--- build_staticfiles.sh
+++ build_staticfiles.sh
@@ -1,10 +1,3 @@
-python3 --version
-pip --version 
-
-cat /etc/os-release
-python3 -m pip install pip==23.0.1
-
-pip install "urllib3<2"
-pip install -r requirements.txt
-
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic
 python3 manage.py collectstatic --no-input
