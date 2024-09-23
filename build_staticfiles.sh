74c722126001c5a492da1b2e5a25e28f0b5d0741
Matheus Bento <matheus51bento@gmail.com>
2024-09-23 16:13:06 -0300

    vercel config again

--- build_staticfiles.sh
+++ build_staticfiles.sh
@@ -1,5 +1,4 @@
-apt-get install sqlite3 libsqlite3-dev
-
+python3 -m pip install "urllib3<2"
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic
 python3 manage.py collectstatic --no-input
