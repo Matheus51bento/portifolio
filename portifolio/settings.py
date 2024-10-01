4b9a5d188af09d0501791db2f894717b1c5f08c7
Matheus Bento <matheus51bento@gmail.com>
2024-10-01 16:03:14 -0300

    fix: image width

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -133,9 +133,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, "/projetos/")
-# MEDIA_URL = os.path.join(BASE_DIR, '/projetos/')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
