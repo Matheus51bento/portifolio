2d35d6517585a2861db1e28d5a63f1bf4ca6b2c5
Matheus Bento <matheus51bento@gmail.com>
2024-10-01 10:22:59 -0300

    feat: postgres

--- build_staticfiles.sh
+++ build_staticfiles.sh
@@ -1,4 +1,12 @@
-python3 -m pip install "urllib3<2"
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic
+python3 --version
+pip --version 
+
+cat /etc/os-release
+python3 -m pip install pip==23.0.1
+pip install psycopg2-binary
+
+pip install "urllib3<2"
+pip install -r requirements.txt
+
+# Build staticfiles
 python3 manage.py collectstatic --no-input
